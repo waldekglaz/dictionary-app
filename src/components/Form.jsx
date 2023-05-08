372e89d2cf80e5f3c10d681c29a3e62127ae3607
Waldemar Głaz <w.glaz@cheil.com>
2023-05-08 12:57:25 +0200

    useContext in Header and Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchIcon from "./../assets/images/icon-search.svg";
+import { formContext } from "../App";
 
-const Form = (props) => {
+const Form = () => {
+  const { onFormSubmit, userInput, setIsFontWindowOpen, onInputChange, isInputEmpty } = useContext(formContext);
   return (
-    <form className="w-full relative" onSubmit={(e) => props.onFormSubmit(e)}>
-      <input onClick={() => props.fontWindowOpen(false)} className="bg-slate-100 w-full tex md:text-xl font-bold py-5 px-6 rounded-3xl dark:text-white dark:bg-slate-800" type="text" value={props.input} onChange={props.onInputChange} placeholder="Search for any word…" />
+    <form className="w-full relative" onSubmit={(e) => onFormSubmit(e)}>
+      <input onClick={() => setIsFontWindowOpen(false)} className="bg-slate-100 w-full tex md:text-xl font-bold py-5 px-6 rounded-3xl dark:text-white dark:bg-slate-800" type="text" value={userInput} onChange={onInputChange} placeholder="Search for any word…" />
       <img className="absolute inset-y-1/3 right-8" src={SearchIcon} aria-hidden="true" />
-      {props.isInputEmpty && <div className="text-red-500 text-xs">Whoops, can’t be empty…</div>}
+      {isInputEmpty && <div className="text-red-500 text-xs">Whoops, can’t be empty…</div>}
     </form>
   );
 };
